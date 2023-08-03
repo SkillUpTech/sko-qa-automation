57f0be6ca4772076f6e367dcbb4105bca18b3d2f
hemamalini <hemamalini@skillup.tech>
2023-08-03 10:27:10 +0530

    recent process

---
+++ src/main/java/com/regression/utility/Constants.java
@@ -0,0 +1,10 @@
+package com.regression.utility;
+
+import org.apache.poi.xssf.usermodel.XSSFColor;
+
+public class Constants
+{
+    public static XSSFColor GREEN = new XSSFColor(new byte[] { (byte) 0, (byte) 128, (byte) 0 });
+    
+    public static XSSFColor RED = new XSSFColor(new byte[] { (byte) 255, (byte) 0, (byte) 0 });
+}
