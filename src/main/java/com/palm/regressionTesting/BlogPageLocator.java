54581b497161cc18038b617b6f4ad9f3340f34e7
hemamalini <hemamalini@skillup.tech>
2024-03-15 17:51:20 +0530

    updated

---
+++ src/main/java/com/palm/regressionTesting/BlogPageLocator.java
@@ -0,0 +1,12 @@
+package com.palm.regressionTesting;
+
+import org.openqa.selenium.WebDriver;
+
+public class BlogPageLocator {
+WebDriver driver;
+	
+	public BlogPageLocator(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+}
